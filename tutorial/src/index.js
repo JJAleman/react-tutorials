ee738342e7e9a9ebebf16fc8550384927159283e
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-21 23:27:02 -0600

    code: added title

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,7 +5,11 @@ import ReactDom from "react-dom";
 // always return JSX
 
 function Greeting() {
-  return <h4>Hello this is John and this is my first component</h4>;
+  return;
+  <div>
+    <h1>This is a title!</h1>
+    <h4>Hello this is John and this is my first component</h4>
+  </div>;
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
