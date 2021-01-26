e29ec417c9d4741fd796a5e2ebaeec1804baa2dc
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-25 23:46:35 -0600

    code: switched title

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,11 +5,13 @@ import ReactDom from "react-dom";
 // always return JSX
 
 function Greeting() {
-  return;
-  <div>
-    <h1>This is a title!</h1>
-    <h4>Hello this is John and this is my first component</h4>
-  </div>;
+  return(
+    <div>
+    <h1>Title will go here</h1>
+    <h4>Hello this is John and this is my first component, I will continue working on this tomorrow!!</h4>
+  </div>
+  )
+  
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
