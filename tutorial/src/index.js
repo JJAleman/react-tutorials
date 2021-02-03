31ed446529ee217d030755d831d094636f102b3a
Juan Aleman <juan.j.aleman@gmail.com>
2021-02-02 23:36:59 -0600

    h2 added

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -12,6 +12,7 @@ function Greeting() {
     <div>
     <h1>Title will go here</h1>
     <h4>Hello this is John and this is my first component, I will continue working on this tomorrow!!</h4>
+    <h2>I promise I will learn react. Better!</h2>
   </div>
   )
   
