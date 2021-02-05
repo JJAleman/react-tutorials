2470b9423b4e59ccd2715710072e69f58f59efeb
Juan Aleman <juan.j.aleman@gmail.com>
2021-02-04 22:52:22 -0600

    added things to first component

---
+++ tutorial/src/components/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+           Hi from inside the component 
+        </div>
+    );
+}
+
+export default index;
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-
+import Index from "./components/index"
 // stateless functional component
 // always return JSX
 
@@ -13,6 +13,7 @@ function Greeting() {
     <h1>Title will go here</h1>
     <h4>Hello this is John and this is my first component, I will continue working on this tomorrow!!</h4>
     <h2>I promise I will learn react. Better!</h2>
+    <Index />
   </div>
   )
   
