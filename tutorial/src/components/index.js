92628d156dae598af3670d6ee5c8206cfb072b80
Juan Aleman <juan.j.aleman@gmail.com>
2021-02-08 21:00:00 -0600

    code: added title

--- tutorial/src/components/index.js
+++ tutorial/src/components/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 const index = () => {
     return (
         <div>
+            <h1>This will be a title</h1>
            Hi from inside the component 
         </div>
     );
