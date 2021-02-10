2e1e6f265019598c031de3541366599d0061b06b
Juan Aleman <juan.j.aleman@gmail.com>
2021-02-10 15:38:31 -0600

    JSX rules

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,20 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Index from "./components/index"
+// import Index from "./components/index"
+
+
 // stateless functional component
 // always return JSX
 
 // **********JSX Rules ***********
 // 1. Always return a single element
+// 2. Div/ section /article or fragment
+// 3. use camelCase for html attribute
+// 4. className instead of class
+// 5. close every Element
+// 6. formatting
 
 function Greeting() {
   return(
-    <div>
-    <h1>Title will go here</h1>
-    <h4>Hello this is John and this is my first component, I will continue working on this tomorrow!!</h4>
-    <h2>I promise I will learn react. Better!</h2>
-    <Index />
-  </div>
+   <div >
+     <h3>hello people</h3>
+     <ul>
+       <li>
+         <a href="">hello world</a>
+       </li>
+     </ul>
+   </div>
   )
   
 }
