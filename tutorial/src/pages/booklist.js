0bd73398cbe4bbc601a4167640d19bb25f5546fa
Juan Aleman <juan.j.aleman@gmail.com>
2021-02-17 17:22:28 -0600

    proper list

--- tutorial/src/pages/booklist.js
+++ tutorial/src/pages/booklist.js
@@ -17,28 +17,44 @@ const books = [
     title: "I Love You to the Moon and Back",
     author: "Amelia Hepworth",
   },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91Fqo5QznlL._AC_UL200_SR200,200_.jpg",
+    title: "Firefly Lane: A Novel",
+    author: "Kristin Hannah",
+  },
 ];
 
-const names = ["john", "david", "benny"];
+// ********* Example of how you can render an array or list *******
 
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>
-  console.log(name);
-})
-console.log(newNames)
+// const names = ["john", "david", "benny"];
+
+// const newNames = names.map((name) => {
+//   return <h1>{name}</h1>
+//   console.log(name);
+// })
+// console.log(newNames)
+//*****************************************************************
 
 function Booklist() {
   return (
     <div>
       <h1>Book List</h1>
-      <section className="booklist">{newNames}</section>
+      <section className="booklist">
+        {books.map((book) => {
+          const { img, title, author } = book;
+          return (
+            <Book book={book}></Book>
+          );
+        })}
+      </section>
     </div>
   );
 }
 
 const Book = (props) => {
   // destructuting props
-  const { img, title, author, children } = props;
+  const { img, title, author } = props.book;
 
   return (
     <article className="book">
